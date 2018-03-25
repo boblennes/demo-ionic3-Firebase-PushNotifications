2af14936185e7a6ac8d6c9b867915e44aaf0fc27
Miguel Zdanovich <miguelangel.zdanovich@personal.com.ar>
2018-03-25 17:49:18 -0300

    Se agrego phonegap-plugin-push y resorces inon, splsh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -11,12 +12,43 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+  constructor(
+    platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen,
+    private push: Push) 
+    {
+      platform.ready().then(() => {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        statusBar.styleDefault();
+        splashScreen.hide();
+        this.pushSetup();
     });
   }
+
+  pushSetup(){
+    const options: PushOptions = {
+      android: {
+        senderID: '508030057444'
+      },
+      ios: {
+          alert: 'true',
+          badge: true,
+          sound: 'false'
+      },
+      windows: {},
+      browser: {
+          pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+      }
+   };   
+   const pushObject: PushObject = this.push.init(options);
+
+   pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+   pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+   pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+  }
+
 }
+
+
